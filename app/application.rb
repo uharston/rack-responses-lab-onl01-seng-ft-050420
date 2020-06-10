cf9adbc097a3d73b601a2af010e8ca5d60317151
Uriah Harston <uharston@gmail.com>
2020-06-10 20:06:51 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
+    if Time.now.hour > 11
+     resp.write  "Good Afternoon!"
+    elsif Time.now.hour < 12 
+     resp.write  "Good Morning!"
+
 
     resp.finish
   end
